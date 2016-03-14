fc57911ef7c3ccc221464a6127f6a2b60e1e9d5b
liliuyang <525826685@qq.com>
2016-03-14 14:47:25 +0800

    指定method属性及使用通配符

---
+++ method/src/lee/action/LoginRegistAction.java
@@ -0,0 +1,60 @@
+package lee.action;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by liliuyang on 2016/3/14.
+ */
+public class LoginRegistAction extends ActionSupport {
+    private String username;
+    private String password;
+    private String tip;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    public String regist()throws Exception
+    {
+        ActionContext.getContext().getSession().put("user",getUsername());
+        setTip("恭喜你"+getUsername()+"注册成功");
+        System.out.println("访问到了");
+        return SUCCESS;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        if(getUsername().equals("li")&&getPassword().equals("li"))
+        {
+            ActionContext.getContext().getSession().put("user",getUsername());
+            setTip("欢迎"+getUsername()+"登录成功");
+            return SUCCESS;
+        }
+        else
+        {
+            setTip("登录失败");
+            return ERROR;
+        }
+    }
+}
